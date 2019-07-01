c9024c8cb54a65a929cb88779481229eed3db59a
rukbate <rukbate@hotmail.com>
2019-07-01 13:13:15 +0800

    print group and weight info

--- src/usr/local/sbin/start.sh
+++ src/usr/local/sbin/start.sh
@@ -54,11 +54,13 @@ do
     GROUP_ID=`echo "$VAR" | sed -r "s/GROUP_(.*)=.*/\1/"`
     GROUP_MEMBERS=`echo "$VAR" | sed 's/.*=//'`
     echo "group.${GROUP_ID}=${GROUP_MEMBERS}" >> /opt/zookeeper/conf/zoo.cfg
+    echo "group.${GROUP_ID}=${GROUP_MEMBERS}"
   fi
   if [[ $VAR =~ ^WEIGHT_[0-9]+= ]]; then
     SERVER_ID=`echo "$VAR" | sed -r "s/WEIGHT_(.*)=.*/\1/"`
     SERVER_WEIGHT=`echo "$VAR" | sed 's/.*=//'`
     echo "weight.${SERVER_ID}=${SERVER_WEIGHT}" >> /opt/zookeeper/conf/zoo.cfg
+    echo "weight.${SERVER_ID}=${SERVER_WEIGHT}"
   fi
 done
 
